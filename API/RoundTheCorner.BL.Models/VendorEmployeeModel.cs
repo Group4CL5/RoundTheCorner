7e84547518ed481769d70d3fa637aaa01744ffdc
Francisco Santillana <francisco.santillana@outlook.com>
2020-04-01 14:06:43 -0400

    Review, VendorEmployee

---
+++ API/RoundTheCorner.BL.Models/VendorEmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundTheCorner.BL.Models
+{
+    public class VendorEmployeeModel
+    {
+        public int id { get; set; }
+        public int userID { get; set; }
+
+        public int vendorID {get;set;}
+    }
+}
